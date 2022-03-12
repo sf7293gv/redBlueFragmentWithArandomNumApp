66125557bd6ab209bdccd71bae92de4c66a3d4cb
Sonic <sf7293gv@go.minnstate.edu>
2022-03-11 21:42:18 -0600

    Modified the blue fragment so it can show the random number when it is replaces the red fragment

--- app/src/main/java/com/example/redbluefragments_randomnumber/BlueFragment.kt
+++ app/src/main/java/com/example/redbluefragments_randomnumber/BlueFragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 
 /**
@@ -15,13 +16,25 @@ import android.view.ViewGroup
  */
 class BlueFragment : Fragment() {
 
+    private lateinit var displayRandomTextView: TextView
 
+    private val randomNumberViewModel: RandomNumberViewModel by lazy {
+        ViewModelProvider(requireActivity()).get(RandomNumberViewModel::class.java)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blue, container, false)
+        val view =  inflater.inflate(R.layout.fragment_blue, container, false)
+        displayRandomTextView = view.findViewById(R.id.random_number_text_view)
+        showRandomNumber()
+        return view
+    }
+
+    private fun showRandomNumber() {
+        val randomNumber = randomNumberViewModel.randomNumber
+        displayRandomTextView.text = randomNumber.toString()
     }
 
     companion object {
--- app/src/main/java/com/example/redbluefragments_randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/redbluefragments_randomnumber/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.setFragmentResultListener(RANDOM_NUMBER_GENERATED, this) {
+            requestKey, bundle ->
+                // swap out red fragment for blue
+            supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, BlueFragment.newInstance()).addToBackStack("BLUE").commit()
+
+            // pause red fragment, add blue fragment on top of it
+
+        }
     }
 }
