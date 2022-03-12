0593b6302a24ad22a38de8bc580406fb5db545cf
Sonic <sf7293gv@go.minnstate.edu>
2022-03-11 21:26:32 -0600

    Copied the fragments from the other app, and added a viewmodel that will store the random number so it can be moved from a fragment to the other

---
+++ app/src/main/java/com/example/redbluefragments_randomnumber/BlueFragment.kt
@@ -0,0 +1,37 @@
+package com.example.redbluefragments_randomnumber
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlueFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlueFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blue, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() = BlueFragment()
+
+    }
+}
